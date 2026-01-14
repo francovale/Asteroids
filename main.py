848ec2232758abda4b9e4d1be50aa82d7266d422
francovale <15008232+francovale@users.noreply.github.com>
2026-01-14 01:33:50 -0500

    player character added, currently static in the middle of the screen

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.fill("black")
-    log_state()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
+        log_state()
+        player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)/1000
         pygame.display.flip()
 
 
